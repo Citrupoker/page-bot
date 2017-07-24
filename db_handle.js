d22306acd961092cb2bb288cac70f7c5b70ce5d5
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-07-24 07:59:38 +0000

    Fixing nedb bug

--- db_handle.js
+++ db_handle.js
@@ -5,7 +5,7 @@ db.find({}, function(err, ads) {
  
  for(var x = 0; x < ads.length; x++){
     console.log(ads[x].title)
-     $('#ads').append(`<li id="` + ads[x].title.split(' ').join('_') + `">` + ads[x].title + ` <button class="text-danger" id="delete" onclick="deleteAd(${ads[x].title.split(' ').join('_')})">x</button></li>`)
+     $('#ads').append('<li id="' + ads[x].title.split(' ').join('_') + '">' + ads[x].title + ' <button class="text-danger" id="delete" onclick="deleteAd(' + ads.title.split(' ').join('_') + ')">x</button></li>');
  }
 });
 
@@ -28,7 +28,7 @@ db.find({}, function(err, ads) {
          db.insert(ad, function (err, newAd) {   // Callback is optional 
            if (err) throw err;
            console.log('Added new ad: ' + newAd.title);
-           $('#ads').append(`<li id="${title.split(' ').join('_')}">` + title + ` <button class="text-danger" id="delete" onclick="deleteAd(${title.split(' ').join('_')})">x</button></li>`);
+           $('#ads').append('<li id="' + title.split(' ').join('_') + '">' + title + ' <button class="text-danger" id="delete" onclick="deleteAd(' + title.split(' ').join('_') + ')">x</button></li>');
          });
          
          $('#title').val('')
